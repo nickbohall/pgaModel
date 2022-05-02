29db37c14c74c2392603435a5942eb6f66544c68
Sleepycornbread <bohall.nick@gmail.com>
2022-05-02 15:40:36 -0500

    added structure

---
+++ pgaDataClean.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import json
+import requests
+
+from pgaScrape import *
+
---
+++ pgaScrape.py
@@ -0,0 +1,21 @@
+#This file will be used for scraping https://www.pgatour.com/ for data
+
+#Import scraping modules
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+#Import data manipulation modules
+import pandas as pd
+import numpy as np
+
+#Dataviz
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+#other
+import json
+import requests
+
+
+
+

86e126538c0f25df32111a86146cb1ab0d2fce5c
Sleepycornbread <bohall.nick@gmail.com>
2022-05-24 18:34:20 -0500

    one api page correct

--- courseMapping.py
+++ courseMapping.py
@@ -1,29 +1,28 @@
 #Import scraping modules
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
+import scrapy
+import json
 
 #Import data manipulation modules
 import pandas as pd
 import numpy as np
-import re
-
-courseList = []
-tourneyListformatted = []
 
 url = 'https://api.datagolf.ca/dg-api/v1/get_ch_data?callback=callback&course_num=ch_14&_=1653426856474'
-r = requests.get(url).text
-soup = BeautifulSoup(html_text, 'html.parser') #us BS to parse html
+html_text = requests.get(url).text #goes to the url
+soup = BeautifulSoup(html_text, 'lxml') #us BS to parse html
+body = soup.find('p').text.split('callback')[1][2:-3]
+# ooi = json.loads(body)
+split = body.split('}, ')[:-1]
+course_dict = []
+for ele in split:
+    ele += '}'
+    cdict = json.loads(ele)
+    course_dict.append(cdict)
+     
+players = []
+for player in course_dict:
+    player_name = player['player_name']
+    players.append(player_name)
 
-
-
-# tourneys = soup.find('body').find('div', class_='wrapper').find('div', class_='container-fluid').find('div', class_='table-container')
-# print(tourneys)
-# for tourney in tourneys:
-#     tourn = tourney.find('br').text.replace(' ', '').strip().split(',')[0]
-#     courseList.append(tourn)
-# print(courseList)
-# for tourney in tourneyList: 
-#     tourney = '-'.join(tourney.split()).lower()
-#     tourney = tourney.replace('\'', '') #get rid of ' for url
-#     tourneyListformatted.append(tourney)
+print(players)
